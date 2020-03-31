dbf35f5641c4e3037d4d82dc0ecbbe8b8fcd8530
BugorSA <vova9840@yandex.ru>
2020-03-31 22:45:54 +0400

    Добавление models для прейскуранта

--- sewingStudio/apps/services/models.py
+++ sewingStudio/apps/services/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    service_name = models.CharField('название услуги', max_length=100)
+    service_description = models.TextField('описание услуги')
+    service_price = models.FloatField('цена услуги')
+
+
+class SubServices(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    sub_service_name = models.CharField('название под-услуги', max_length=100)
+    sub_service_description = models.TextField('описание под-услуги')
+    sub_service_price = models.FloatField('цена под-услуги')
