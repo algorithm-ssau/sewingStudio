176da7da5e1f956792c2670038ff873bda47e6da
BugorSA <vova9840@yandex.ru>
2020-06-08 18:04:51 +0400

    исправление ошибок

--- sewingStudio/settings.py
+++ sewingStudio/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
