1b6893100a3bb86c2b1584d8bd5c193b5d11ff9b
BugorSA <vova9840@yandex.ru>
2020-06-08 17:04:56 +0400

    исправление ошибок

--- sewingStudio/wsgi.py
+++ sewingStudio/wsgi.py
@@ -15,6 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sewingStudio.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
